379d059d777b28ed222e6059eb83494bd415ce87
priyanka <bharadwajpriyanka204@gmail.com>
2023-03-05 18:52:26 -0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:age_cal/home.dart';
+import 'package:flutter/material.dart';
+
+
+void main()
+{
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:Age(),
+    ),
+  );
+}
